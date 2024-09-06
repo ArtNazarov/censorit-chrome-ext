0d15459a79adf140cf644df1dfa4dfa94e0d5404
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2024-09-06 12:14:12 +0500

    rename

--- extension/ext/js/dialog.js
+++ extension/ext/js/dialog.js
@@ -10,7 +10,7 @@ const AuctionUrl = 'https://raw.githubusercontent.com/ArtNazarov/censorit-chrome
 const PrisonUrl = 'https://raw.githubusercontent.com/ArtNazarov/censorit-chrome-ext/main/blocklist_keywords/prison.txt';
 const FraudUrl = 'https://raw.githubusercontent.com/ArtNazarov/censorit-chrome-ext/main/blocklist_keywords/fraud.txt';
 const WeaponUrl = 'https://raw.githubusercontent.com/ArtNazarov/censorit-chrome-ext/main/blocklist_keywords/weapon.txt';
-const NegativeUrl = 'https://raw.githubusercontent.com/ArtNazarov/censorit-chrome-ext/main/blocklist_keywords/negative.txt';
+const NegativeUrl = 'https://raw.githubusercontent.com/ArtNazarov/censorit-chrome-ext/main/blocklist_keywords/negatives.txt';
 
 async function getLines(url) {
   try {
